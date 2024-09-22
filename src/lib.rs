80c1430da86c7a4228a1aeedcc77c83d85308733
nomadic4life <orbison.dennis@gmail.com>
2024-09-22 01:14:02 -0700

    added scaling values to the claim algo

--- src/lib.rs
+++ src/lib.rs
@@ -56,10 +56,16 @@ impl StateAccount {
     pub const DEFUALT_AMOUNT: u64 = 1;
     pub const INIT_VALUE: u64 = 1_000_000;
 
-    pub fn claim(&mut self) -> ProgramResult {
+    // there is a potential bug when USDC value is 0.000_001
+    pub fn claim(&mut self, amount: u64) -> ProgramResult {
         let clock = Clock::get()?;
         let distance = clock.slot - self.last_slot;
         let depth = distance / StateAccount::TARGET as u64;
+
+        let multiplier = if self.next_value <= 10 { 4 } else { 2 };
+        let scale: u8 = if self.next_value <= 10 { 2 } else { 1 };
+        let off_set = if self.next_value <= 10 { 1 } else { 0 };
+        // let divisor = if self.next_value < 100 { 1000 } else { 100 };
 
         let (
             // EXTRACTED VALUES
@@ -68,9 +74,11 @@ impl StateAccount {
             next_value,
             next_height,
             transfer_amount,
+            pool_amount,
         ) = if self.next_height == 0 {
             let next_height = self.next_height + 1;
             let next_value = StateAccount::INIT_VALUE;
+            let pool_amount = 0;
 
             (
                 StateAccount::RESET,
@@ -78,10 +86,12 @@ impl StateAccount {
                 next_value,
                 next_height,
                 StateAccount::DEFUALT_AMOUNT,
+                pool_amount,
             )
         } else if depth == 0 {
-            let next_value = self.next_value * 2;
+            let next_value = self.next_value * multiplier;
             let next_height = self.next_height + 1;
+            let pool_amount = 0;
 
             (
                 self.next_value,
@@ -89,34 +99,41 @@ impl StateAccount {
                 next_value,
                 next_height,
                 StateAccount::DEFUALT_AMOUNT,
+                pool_amount,
             )
         } else if self.next_height > depth as u8 {
-            let value = self.next_value >> depth;
+            let value = (self.next_value >> (depth * scale as u64)) - off_set;
             let current_value = value + value * depth / 100;
             let current_height = self.next_height - depth as u8;
             let next_height = current_height + 1;
             let next_value = value * 2;
+            let transfer_amount = if amount > depth { depth } else { amount };
+            let pool_amount = depth - transfer_amount;
 
             (
                 current_value,
                 current_height,
                 next_value,
                 next_height,
-                depth,
+                transfer_amount,
+                pool_amount,
             )
         } else {
-            let value = self.next_value >> self.next_height;
+            let value = (self.next_value >> (self.next_height * scale)) - off_set;
             let current_value = 0;
             let next_value = value + value * depth / 100;
             let current_height = 0;
             let next_height = 1;
+            let transfer_amount = if amount > depth { depth } else { amount };
+            let pool_amount = depth - transfer_amount;
 
             (
                 current_value,
                 current_height,
                 next_value,
                 next_height,
-                depth,
+                transfer_amount,
+                pool_amount,
             )
         };
 
@@ -127,6 +144,9 @@ impl StateAccount {
         self.next_height = next_height;
         self.next_value = next_value;
         self.prev_height = self.next_height;
+
+        // if pool_amount != 0
+        // transfer / mint pool amount to pool
 
         // if last_value != 0
         // transfer current value
@@ -258,7 +278,8 @@ pub fn process_claim(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramRe
         return Err(ProgramError::Custom(ErrorCode::InvalidAccountType as u32));
     }
 
-    account_data.claim()
+    let amount = 1;
+    account_data.claim(amount)
 }
 
 // lib
