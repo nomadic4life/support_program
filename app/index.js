21c3c2c1473bc7eb867689149135a3d38fb52216
nomadic4life <orbison.dennis@gmail.com>
2024-09-22 09:34:35 -0700

    extended initialize instruction, creates a mint 2022 token, was a real pain to debug

--- app/index.js
+++ app/index.js
@@ -1,2 +1,110 @@
-import web3 from "@solana/web3.js"
+import {
+    PublicKey,
+    Keypair,
+    Connection,
+    LAMPORTS_PER_SOL,
+    Transaction,
+    TransactionInstruction,
+    SystemProgram,
+    sendAndConfirmRawTransaction,
+    // TransactionConfirmationStrategy,
+} from "@solana/web3.js"
+// const web3 = require("@solana/web3.js")
+
+console.log("sanity check")
+// console.log(web3)
+
+const programId = new PublicKey("Bsygg6pgkUnupUAw1QcofEqUNEhYpkn7rZ3u3SUbDvAq");
+const tokenProgramId = new PublicKey("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb");
+// const tokenProgramId = new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
+
+
+const keypair = Keypair.generate();
+
+const connection = new Connection("http://127.0.0.1:8899")
+
+console.log(programId, tokenProgramId)
+
+const run = async () => {
+
+    const latestBlockhash = await connection.getLatestBlockhash();
+    const signature = await connection.requestAirdrop(keypair.publicKey, LAMPORTS_PER_SOL);
+    const response = await connection.confirmTransaction({
+        signature,
+        ...latestBlockhash,
+    });
+
+    console.log({ response })
+
+    const [stateAccount] = PublicKey.findProgramAddressSync([
+        Buffer.from("state")
+    ], programId)
+
+    // const tokenMint = Keypair.generate();
+
+    const [tokenMint] = PublicKey.findProgramAddressSync([
+        Buffer.from("token-mint")
+    ], programId)
+
+    const transaction = new Transaction({ ...latestBlockhash });
+
+    const instruction = new TransactionInstruction({
+        data: [0],
+        keys: [
+            {
+                isSigner: true,
+                isWritable: true,
+                pubkey: keypair.publicKey,
+            },
+            {
+                isSigner: false,
+                isWritable: true,
+                pubkey: stateAccount,
+            },
+
+            {
+                isSigner: false,
+                isWritable: false,
+                pubkey: tokenProgramId,
+            },
+            {
+                isSigner: false,
+                isWritable: true,
+                pubkey: tokenMint,
+            },
+            // {
+            //     isSigner: false,
+            //     isWritable: true,
+            //     pubkey: tokenMint.publicKey,
+            // },
+            {
+                isSigner: false,
+                isWritable: false,
+                pubkey: keypair.publicKey,
+            },
+            {
+                isSigner: false,
+                isWritable: false,
+                pubkey: SystemProgram.programId,
+            },
+        ],
+        programId,
+    });
+
+    transaction.add(instruction);
+    transaction.sign(keypair);
+    // transaction.feePayer = keypair.publicKey;
+
+
+    console.log("SIGNER: ", keypair, keypair.publicKey, keypair.publicKey.toString());
+    console.log(transaction);
+
+
+    const sig = await connection.sendTransaction(transaction, [keypair], "finalized");
+    // const sig = await sendAndConfirmRawTransaction(connection, transaction, [keypair]);
+    console.log({ sig })
+}
+
+
+run()
 
